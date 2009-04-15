858618b1a9c55e08bd1ad8ea57773d8fe2d9cc23
Pavol Juhas <pavol.juhas@gmail.com>
2009-04-15 22:56:21 +0000

    Improved statistic counting script.

--- pybin/download_index.py
+++ pybin/download_index.py
@@ -1,15 +1,26 @@
 import os
+import re
+import time
 
 _thisdir = os.path.dirname(os.path.abspath(__file__))
 _diffpydir = os.path.dirname(_thisdir)
+_statsfile = os.path.join(_diffpydir, 'stats.txt')
 
-def _getDownloadList():
-    import re
-    downloaddir = os.path.join(_diffpydir, 'download')
+rv = ''
+
+def _getDownloadList(drel):
+    """Obtain data needed for creating download table for a directory.
+
+    drel    -- directory path relative to _diffpydir
+
+    Return dictionary with items
+    ('mtime', 'name', 'dcount', 'fmtsize', 'size', 'fmtmtime')
+    """
+    downloaddir = os.path.join(_diffpydir, drel)
     ignore = re.compile(r'^[.]|stats.txt')
     lst = []
     for f in os.listdir(downloaddir):
-	relpath = os.path.join('download', f)
+	relpath = os.path.join(drel, f)
 	fullpath = os.path.join(downloaddir, f)
 	if not os.path.isfile(fullpath) or ignore.match(f): continue
 	lst.append({
@@ -23,18 +34,17 @@ def _getDownloadList():
     lsort = [(-d['mtime'], d['name'], d) for d in lst]
     lsort.sort()
     lst = [ tpl[-1] for tpl in lsort ]
-    # add dcount field
-    statsfile = os.path.join(downloaddir, 'stats.txt')
+    # add the dcount field
     dcount = {}
-    if os.path.isfile(statsfile):
-        for line in open(statsfile):
-            w = line.split()
+    if os.path.isfile(_statsfile):
+        for line in open(_statsfile):
+            w = line.lstrip('/').split()
+            if len(w) != 2:     continue
             dcount[w[0]] = int(w[1])
     for d in lst:
-        d['dcount'] = dcount.get(d['name'], 0)
+        d['dcount'] = dcount.get(d['relpath'], 0)
     # add fmtsize and fmtmtime fields
     from pathutils import formatbytes
-    import time
     for d in lst:
         d['fmtsize'] = formatbytes(d['size'],
                 bytename='', kiloname='K', meganame='M')
@@ -47,8 +57,8 @@ def _getNameWidth(lst):
     width = 4 * (width/4 + 1)
     return width
 
-def _getDownloadHTMLCode():
-    lst = _getDownloadList()
+def _getDownloadHTMLCode(drel):
+    lst = _getDownloadList(drel)
     nw = _getNameWidth(lst)
     fmt = "%-@NWs%-20s%-8s%s".replace('@NW', str(nw))
     header = ("<pre>" + fmt + '<hr>') % \
@@ -64,7 +74,14 @@ def _getDownloadHTMLCode():
     return code
 
 def index(req):
-    return _getDownloadHTMLCode()
+    return download(req)
+
+def download(req):
+    return _getDownloadHTMLCode('download')
+
+def packages(req):
+    return _getDownloadHTMLCode('packages')
+
 
 #   <pre>Name                    Last modified      Size  Description<hr><a href="diffpy-1.0b.1232.tgz">diffpy-1.0b.1232.tgz</a>    14-May-2007 21:00  6.9M  
 #   <a href="diffpy-1.0b.1232.exe">diffpy-1.0b.1232.exe</a>    14-May-2007 21:00   20M  
